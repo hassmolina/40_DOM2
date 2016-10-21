f4a4ab2c0c0650ab6e709e911ad5604ec8869ccf
hassmolina <hassmolinagutierrez@gmail.com>
2016-10-21 16:00:55 -0500

    ejercicio

--- js/javascript.js
+++ js/javascript.js
@@ -3,17 +3,21 @@ function todoList() {
     var text = document.createTextNode(item);
     var newItem = document.createElement("li");
     newItem.style.backgroundColor = "#17d487";
-    
-    newItem.style.fontFamily = "Helvetica";
+    newItem.style.fontFamily = "Meyrio";
+    newItem.style.fontSize = "20px";
     var checkbox = document.createElement('input');
     checkbox.type = "checkbox";
     checkbox.value = 1;
     document.getElementById('todoList').appendChild(newItem);
     var icono = document.createElement("i");
     icono.setAttribute("class", "fa fa-trash");
+    icono.style.marginLeft = "300px";
     newItem.appendChild(checkbox);
     newItem.appendChild(text)
     newItem.appendChild(icono);
+
+//TEXTO EN BLANCO
+
 
     //ELIMINAR
     icono.onclick = function() {
